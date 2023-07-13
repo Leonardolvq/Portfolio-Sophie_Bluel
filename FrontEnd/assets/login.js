16a4de73f5439973a1ec8e1242ec814261fffcd6
Leonardo Lopes <leonardo.lopes@outlook.fr>
2023-07-13 19:10:40 +0200

    Page login fonctionnelle

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -8,31 +8,26 @@ async function validateForm(){
         console.log(identifiant.value);
         const password = document.getElementById("password");
         console.log(password.value);
-        const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4";
-
         const response = await fetch("http://localhost:5678/api/users/login", {
             method: 'POST',
             headers: {
                 "accept": "application/json",
-                "Authorization": `Bearer ${token}`
-            },
+                "Content-Type": "application/json" },
             body: JSON.stringify({
                 email: identifiant.value,
                 password: password.value
-            }) 
+            })
         })
-        response.then((res) => {
-            if (res.ok) {
-                console.log("co");
-                console.log(`${res}`)
-                window.location.href = "./index.html";
-            } else {
-                console.log("Erreur lors de la requête :", res.status);
-                //errorMessageElement.textContent = "Erreur dans l'identifiant ou le mot de passe.";
-            }
-        }).catch((error) => {
-            console.error("Erreur lors de la requête :", error);
-        });
+        
+        if (response.ok) {
+            let res = await response.json();
+            console.log("co");
+            console.log(`${res}`)
+            window.location.href = "index.html";
+        } else {
+            console.log("Erreur lors de la requête :", response.status);
+            //errorMessageElement.textContent = "Erreur dans l'identifiant ou le mot de passe.";
+        }
         
     })
     
